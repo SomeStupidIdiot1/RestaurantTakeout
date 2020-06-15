59a8f0e50b33f7c0407a3026d798cd710e4a57b8
SomeStupidIdiot1 <somestupididiot1@gmail.com>
2020-06-15 00:28:55 -0400

    more components for layout

---
+++ frontend/src/components/AllItems.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AllItems() {
+  return <div></div>;
+}
+
+export default AllItems;
---
+++ frontend/src/components/ItemForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ItemForm() {
+  return <div></div>;
+}
+
+export default ItemForm;
